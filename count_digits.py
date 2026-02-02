098c72409cc31c86cb5caba3c3be675665b499d7
Shreyansh Gupta <05shreyansh@gmail.com>
2026-02-02 20:01:21 +0530

    commit changes

---
+++ count_digits.py
@@ -0,0 +1,9 @@
+num = int(input("Enter a number: "))
+if num == 0:
+    print("Number of digits:1")
+else:
+    count = 0
+    while num > 0:
+        num = num // 10
+        count += 1
+    print("Number of digits:", count) 
